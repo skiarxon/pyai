b2c138abfc15dfd09ae00a3fcbf7a2b80f4b87ec
skiarxon <skiarxon@gmail.com>
2025-07-02 10:57:43 +0300

    First commit

---
+++ config.py
@@ -0,0 +1,4 @@
+model_name = "gemini-2.0-flash-001"
+WORKING_DIR = "./calculator"
+MAX_ITERS = 20
+
---
+++ functions/get_file_content.py
@@ -0,0 +1,41 @@
+import os
+from google.genai import types
+
+MAX_CHARS = 10000
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    target_dir = abs_working_dir
+    target_file = abs_file_path
+
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(target_file, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        if len(content) > MAX_CHARS:
+            return content[:MAX_CHARS] + f'\n[...File "{file_path}" truncated at 10000 characters]'
+
+        return content
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
